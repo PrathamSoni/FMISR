4ce8eee303871344d59700402623a11aa81f43e3
Pratham Soni <psoni2002@gmail.com>
2019-10-13 12:07:17 -0400

    Update dcsrn_test.py

--- dcsrn_test.py
+++ dcsrn_test.py
@@ -9,29 +9,16 @@ import tensorflow as tf
 
 from tf_dcsrn import dcsrn, image_util
 
+#Testing stream
+
+#checkpoint/tfboard file path
 output_path = "./snapshots/random_e4"
-# path of dataset, here is the HCP dataset
-dataset_HCP = "Train"
-
-#preparing data loading, you may want to explicitly note the glob search path on you data 
-data_provider = image_util.MedicalImageDataProvider()
-
-print("\ndata_provider initialization over.\n")
-
-# setup & training
+# setup
 net = dcsrn.DCSRN(channels=3)
 
 print("\nGraph set over.\n")
 
-trainer = dcsrn.Trainer(net)
-
-print("\nBegin to train.\n")
-
-#path = trainer.train(data_provider, output_path, restore = True)
-
-print("\nTraining process is over.\n")
-
-# verification, randomly test 4 images
+# testing step
 test_provider = image_util.TestDataProvider()
 test_x, test_y = test_provider(30)
 result = net.predict("./snapshots/fixed_e4/model.cpkt", test_x)
